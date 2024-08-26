6bdc03dadd9d7736242dc28a45b8de4330b7cc71
Meyer-Usuga <usugarestrepomeyer@gmail.com>
2024-08-25 19:33:00 -0500

    Se añade validación básica al formulario de contacto

--- javascript/script.js
+++ javascript/script.js
@@ -48,10 +48,9 @@ const hairCuts = [
     },
 ];
 
-const modal_info = document.getElementById("modal-information");
+const modal_info = document.getElementById("modal-info");
 
-function showModal(typeCutId) {
-
+function showInformation(typeCutId) {
     const modalBody = document.getElementById("modal-body");
     let data = hairCuts.find(data => data.id === typeCutId);
 
@@ -69,13 +68,69 @@ function showModal(typeCutId) {
     
     `;
     }
-
     modal_info.showModal();
 }
 
-function closeModal() {
-    modal_info.close();
+function closeModal(idModal) {
+    if(idModal == 'modal-info'){
+        modal_info.close();
+    }
+    else if(idModal == 'modal-alert'){
+        modal_alert.close();
+    }
 }
 
 /** Lógica para la página de contacto */
 
+const names = document.getElementById("txtName");
+const email = document.getElementById("txtEmail");
+const subject = document.getElementById('txtSubject');
+const message = document.getElementById("txtMessage");
+const form = document.getElementById("form");
+const modal_alert = document.getElementById("modal-alert");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+});
+
+function validate() {
+
+    // Cuerpod del modal
+    const modalBody = document.getElementById("modal-body");
+    
+    // Alertas del formulario
+    const alert_name = document.getElementById('alert-name');
+    const alert_email = document.getElementById('alert-email');
+    const alert_subject = document.getElementById('alert-subject');
+    const alert_message = document.getElementById('alert-message');
+    const alert_form = document.getElementById('alert-form');
+
+    // Validación regex para el email
+    let regex =  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+    if (names.value && email.value) {
+        console.log(email.value)
+        if (names.value.length < 6) {
+            alert_name.style.display = 'grid';
+        }
+        else if (!regex.test(email.value)) {
+            alert_email.style.display = 'grid';
+        }
+        else if(subject.value.length < 6){
+            alert_subject.style.display = 'grid';
+        }
+        else if(message.value.length < 6){
+            alert_message.style.display = 'grid';
+        }
+        else{
+            modalBody.innerHTML = `
+            <h3>¡Listo!</h3>
+            El mensaje se envió exitosamente y pronto lo revisaremos.`;
+    
+            modal_alert.showModal();
+        }
+    }
+    else {
+        alert_form.style.display = 'grid';
+    }
+}
